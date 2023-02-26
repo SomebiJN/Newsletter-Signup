2cb9b6bf3d5ce43830aba5155a3e43d2c06d751d
Somebi <sjnwogu@mun.ca>
2023-02-26 06:41:36 -0500

    Changed website

--- app.js
+++ app.js
@@ -1,43 +1,85 @@
-const express = require('express')
-const path = require("path");
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const https = require("https")
+const { options, post } = require("request")
+
 const app = express()
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
-});
+//serve up our static(local) css file and images located in public folder
+app.use(express.static("public"));
+//tell app to use body parser
+app.use(bodyParser.urlencoded({extended: true}))
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
-
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
+//user visits our home page route
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html")
 })
 
-module.exports = app
+//post request made by user to home route for when submit clicked on form
+app.post("/",function(req,res){
+    const firstName = req.body.fName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    //subscriber data sent to mailchimp
+    const data = {
+        members:    [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    //data is sent to mailchimp in JSON format
+    const jsonData = JSON.stringify(data)
+
+    const list_id = "ee6e696ef4"
+    const url = "https://us13.api.mailchimp.com/3.0/lists/" + list_id
+    const options = {
+        method: "post",
+        auth: "somebi:21a102bcec03c8df685cb5d0cfbe288d-us13"
+    }
+
+    //make POST request to mailchimp
+    const request = https.request(url, options, function(response){
+
+        if(response.statusCode === 200){ //OK
+            //send response back to website
+            res.sendFile(__dirname + "/success.html")
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+        response.on("data", function(data){
+            //log response to post request our server sent to mailchimp
+            console.log(JSON.parse(data))
+        })
+    })
+
+    //send user json data to mailchimp server
+    request.write(jsonData)
+    request.end()
+    
+    //apiKey: "21a102bcec03c8df685cb5d0cfbe288d-us13"
+    
+})
+
+//post request made by failure route when Try again button clicked
+app.post("/failure",function(req,res){
+    //redirect to home route to try again
+    res.redirect("/")
+})
+
+//listens on heroku's set port or on local port 3000
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running on port 3000")
+})
+
+//Audience ID
+//ee6e696ef4
