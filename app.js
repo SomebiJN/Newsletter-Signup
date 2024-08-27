495c89891252593f293536ed4e10db25acd458f0
Somebi Nwogu <sjnwogu@mun.ca>
2024-08-27 19:58:48 -0400

    Update app.js
    modified comment

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post("/failure",function(req,res){
     res.redirect("/")
 })
 
-//listens on heroku's set port or on local port 3000
+//listens on platform's set port or on local port 3000
 app.listen(process.env.PORT || 3000,function(){
     console.log("server is running on port 3000")
 })
